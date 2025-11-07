5ac85d7ced55ffde7c0210e34cbe8ec07cc4c4bd
Chris <chris@aicybermedia.com.au>
2025-11-07 12:33:06 +1100

    fix: build issues

--- src/app/auth/workflow/components/nodes/image/image-edit-dialog.tsx
+++ src/app/auth/workflow/components/nodes/image/image-edit-dialog.tsx
@@ -317,7 +317,6 @@ export function ImageEditDialog({ imageUrl, onClose, onSave }: ImageEditDialogPr
         {/* Canvas Area */}
         <div className="flex-1 flex items-center justify-center p-4 overflow-auto bg-gray-100">
           <div className="bg-white shadow-lg">
-            {/* @ts-expect-error - react-konva Stage does support children but types are incorrect */}
             <Stage
               ref={stageRef}
               width={stageSize.width}
@@ -327,7 +326,6 @@ export function ImageEditDialog({ imageUrl, onClose, onSave }: ImageEditDialogPr
               onMouseup={handleMouseUp}
               style={{ cursor: 'crosshair' }}
             >
-              {/* @ts-expect-error - react-konva Layer does support children but types are incorrect */}
               <Layer>
                 {/* Background Image */}
                 <ImageLayer imageUrl={imageUrl} />
